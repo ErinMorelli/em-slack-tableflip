9a46eb52c6cc63812ebbdb3c9bf265e3bcaefd02
Erin Morelli <erin@erinmorelli.com>
2015-08-10 13:13:48 -0400

    Configuring app for heroku

---
+++ tableflip.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# EM Slack Tableflip
+# Copyright (c) 2015 Erin Morelli
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+''' Run the EM Slack Tableflip application
+'''
+
+from wsgiref.handlers import CGIHandler
+from slack_tableflip.app import APP as application
+
+CGIHandler().run(application)
